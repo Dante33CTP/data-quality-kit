b03654417fceceecd24335421737d0c3817cd1ef
Dante33CTP <dre21mayo@hotmail.com>
2024-07-20 13:41:29 -0500

    Add files config

--- setup.py
+++ setup.py
@@ -1,23 +1,38 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as file:
+    long_description = file.read()
 
 setup(
-    name='data-quality-utility-kit',
-    version='0.1',
+    name="data-quality-kit",
+    version="0.1.0",
+    description=" library of functions for managing and improving data quality in Datasets",
     packages=find_packages(),
-    install_requires=[
-        'pandas',
-    ],
-    author='Tu Nombre',
-    author_email='tuemail@example.com',
-    description='A utility kit for data quality analysis.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/tu_usuario/data-quality-utility-kit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dante33CTP/data-quality-kit",
+    author="DantePedrozo",
+    author_email="dante.victor.33@gmail.com",
+    license="Apache License 2.0",
+    keywords="I/O Files",
+    install_requires=["pandas >= 2.2.2", "assertpy >= 1.1.0 ", "pytest >= 8.2.2"],
+    test_suite='tests',
+    tests_require=['pytest'],
     classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ]
 
+)
