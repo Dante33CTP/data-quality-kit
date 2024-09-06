40ef90e56033cd4e7146acdcb3cca1bfe31fa8fd
Dante33CTP <dre21mayo@hotmail.com>
2024-09-06 11:55:46 -0500

    Fix: rename parameter type

--- data_quality_kit/validity.py
+++ data_quality_kit/validity.py
@@ -1,7 +1,7 @@
-import pandas as pd
+from pandas import DataFrame
 
 
-def assert_that_there_are_not_nulls(df: pd.DataFrame, field_name: str) -> bool:
+def assert_that_there_are_not_nulls(df: DataFrame, field_name: str) -> bool:
     """
     Checks for null values in a specified column of a DataFrame.
 
