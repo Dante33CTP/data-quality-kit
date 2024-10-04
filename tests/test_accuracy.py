12571eb25a288af866004652060765f2dafe320f
Dante33CTP <dre21mayo@hotmail.com>
2024-10-04 12:03:50 -0500

    Update test names for improved consistency

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -22,26 +22,26 @@ def test_assert_that_type_value_in_nonexistent_column():
     ).is_equal_to(error_msg)
 
 
-def test_assert_that_values_in_catalog_all_values_in_catalog():
+def test_assert_that_values_in_catalog_with_all_values_in_catalog():
     catalog = ['Value1', 'Value2', 'Value3', 'Value4']
     result = assert_that_values_in_catalog(df_global, 'test_column', catalog)
     assert_that(result).is_true()
 
 
-def test_assert_that_values_in_catalog_not_all_values_in_catalog():
+def test_assert_that_values_in_catalog_with_not_all_values_in_catalog():
     catalog = ['Value1', 'Value2', 'Value3']
     result = assert_that_values_in_catalog(df_global, 'test_column', catalog)
     assert_that(result).is_false()
 
 
-def test_assert_that_values_in_catalog_column_not_in_dataframe():
+def test_assert_that_values_in_catalog_with_column_not_in_dataframe():
     error_msg = "Column 'non_existent_column' does not exist in the DataFrame."
     assert_that(assert_that_values_in_catalog).raises(ValueError).when_called_with(
         df_global, 'non_existent_column', ['Value1', 'Value2']
     ).is_equal_to(error_msg)
 
 
-def test_assert_that_values_in_catalog_empty_catalog():
+def test_assert_that_values_in_catalog_with_empty_catalog():
     error_msg = "The catalog is empty."
     assert_that(assert_that_values_in_catalog).raises(ValueError).when_called_with(
         df_global, 'test_column', []
